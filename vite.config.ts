4cadca19b507446382ea1223dfd18d7c9152f77d
Abhishek Bhatnagar <abhishekbhatnagar13x@gmail.com>
2026-09-05 13:40:11 +0530

    Change production base path in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: mode === 'production' ? '/money-manager/' : '/',
+  base: mode === 'production' ? '/Kanjoos_Money_Manager/' : '/',
   plugins: [
     react(),
     VitePWA({
